0e501a7507d9c565fec7a17c3ceaae70c22426be
Anurag Pasayat <pasayatanurag@gmail.com>
2026-06-26 10:34:21 +0530

    added: testimonials, Find Us (mini map), Footer

--- zairza-project/script.js
+++ zairza-project/script.js
@@ -29,3 +29,22 @@ document.getElementById("left").onclick = () => {
         behavior: "smooth"
     });
 };
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 400){
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
